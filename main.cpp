4c7767315cd03eab17b1c45ad1215f3d814cf688
Ryan Grunest <ryangrunest@geeko>
2024-10-04 20:05:42 +0200

    write to file

--- main.cpp
+++ main.cpp
@@ -10,12 +10,17 @@
 
 using namespace std;
 
-void ReadFile(vector<int> &temperatures) {
+void ConvertToCelsius(vector<int> &temperatures) {
   ifstream temperatureFS;
+  ofstream temperatureOFS;
   string location;
   int temperature;
+  int tempInCelsius;
+
 
   temperatureFS.open("FahrenheitTemperature.txt");
+  temperatureOFS.open("CelsiusTemperature.txt");
+
 
   if (!temperatureFS.is_open()) {
     cout << "Could not open file" << endl;
@@ -27,16 +32,25 @@ void ReadFile(vector<int> &temperatures) {
 
       cout << "Location: " << location << endl;
       cout << "Temperature: " << temperature << endl;
+      
+      if (!temperatureFS.fail()) {
+        tempInCelsius = temperature - 32;
+        tempInCelsius = tempInCelsius * 5;
+        tempInCelsius = tempInCelsius / 9;
 
-      temperatures.push_back(temperature);
+        temperatureOFS << location << " " << tempInCelsius << endl;
+
+        temperatures.push_back(temperature);
+      }
     }
 
     temperatureFS.close();
+    temperatureOFS.close();
   }
 }
 
 int main() {
   vector<int> temperatures;
 
-  ReadFile(temperatures);
+  ConvertToCelsius(temperatures);
 }
